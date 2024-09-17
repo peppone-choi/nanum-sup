9771df3def95b658ef4baf3565414ae4d0fdc3ed
peppone-choi <peppone.choi@gmail.com>
2024-09-18 08:33:38 +0900

    [최병호] jsonwebtoken, dotenv 설치

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,22 @@ import appRouter from "./routers/app.router";
 import userRouter from "./routers/users.router";
 import viewRouter from "./routers/views/view.router";
 import adminViewRouter from "./routers/views/adminView.router";
+import dotenv from "dotenv";
 const app = express();
-
+dotenv.config();
 app.use(appRouter);
 app.use(userRouter);
 app.use(viewRouter);
 app.use(adminViewRouter);
-
+const PORT = process.env.PORT || 4000;
 // view 파일들 모아놓는 위치 설정
 app.set("views", path.join(__dirname, "views"));
 // view engine 세팅
 app.set("view engine", "ejs");
 
-app.use(morgan("dev")); // 클로져
-app.use("/static", express.static(path.join(__dirname, "../public")));
+app.use(morgan("dev")); // 로그 찍어주는 미들웨어
+app.use("/static", express.static(path.join(__dirname, "../public"))); // 정적 파일 제공 미들웨어
 
-app.listen(4000, () => {
-  console.log(`Server is running on port 4000`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
