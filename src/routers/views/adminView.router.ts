d8acd8fbfbe5249b461d629b45dbcfaef24fb34f
peppone-choi <peppone.choi@gmail.com>
2024-09-18 13:18:51 +0900

    [최병호] 뷰 파일 정리 및 reset css 적용

--- src/routers/views/adminView.router.ts
+++ src/routers/views/adminView.router.ts
@@ -4,8 +4,10 @@ const adminViewRouter = express.Router();
 
 const BASE_PATH = `/admin/views`;
 
-// adminViewRouter.get(`${BASE_PATH}/login`, (req, res) => {
-//   res.render(`admin/login`);
-// });
+adminViewRouter.get(`${BASE_PATH}/login`, (req, res) => {
+  res.render(`admin/login`, {
+    title: "관리자 로그인",
+  });
+});
 
 export default adminViewRouter;
--- src/routers/views/view.router.ts
+++ src/routers/views/view.router.ts
@@ -4,14 +4,16 @@ const viewRouter = express.Router();
 
 const BASE_PATH = "/views";
 
-// viewRouter.get(`${BASE_PATH}/login`, (req, res) => {
-//   res.render(`login`);
-// });
+viewRouter.get(`${BASE_PATH}/login`, (req, res) => {
+  res.render(`login`, {
+    title: "로그인",
+  });
+});
 
-// viewRouter.get(`${BASE_PATH}`, (req, res) => {
-//   res.render("index", {
-//     title: "홈",
-//   });
-// });
+viewRouter.get(`${BASE_PATH}`, (req, res) => {
+  res.render("index", {
+    title: "홈",
+  });
+});
 
 export default viewRouter;
