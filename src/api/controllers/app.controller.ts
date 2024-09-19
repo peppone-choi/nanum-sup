dde2452c33983b74a563ac60f9f103b045430aee
mingyee3101 <kmingee@daum.net>
2024-09-19 17:46:25 +0900

    일단올리는 PR
    중간에 일이 좀 있어서 실은 작업이 많이 덜되었는데, 이후에 계속 작업할 예정입니다.

--- src/api/controllers/app.controller.ts
+++ src/api/controllers/app.controller.ts
@@ -1,1 +1,13 @@
 import type { Request, Response, NextFunction } from "express";
+
+export default class PostitController {
+    constructor(private readonly _commentController: commentService) {
+        this.createComment = this.createComment.bind(this);
+        this.getComment = this.getComment.bind(this);
+        this.editComment = this.editComment.bind(this);
+        this.deleteComment = this.deleteComment.bind(this);
+    }
+    async createComment(req: Request, res: Response, next: NextFunction){
+    res.send("POST /api/comment")
+    }
+}
