c6c09fa2d15fa295824f94b6f3a39ce72568b2b3
peppone-choi <peppone.choi@gmail.com>
2024-09-19 10:52:52 +0900

    [최병호] 몽구스와 쿠키 파서 설치

---
+++ src/db/mongoose.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const mongodbUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}`;
+
+export const db = mongoose.createConnection(mongodbUri).asPromise();
+
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}`
+    );
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+})();
