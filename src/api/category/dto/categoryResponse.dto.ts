419ff349c1396b49651a7df96a77a35686aa0a15
peppone-choi <peppone.choi@gmail.com>
2024-09-24 11:45:21 +0900

    [최병호] 카테고리 핫픽스

--- src/api/category/dto/categoryResponse.dto.ts
+++ src/api/category/dto/categoryResponse.dto.ts
@@ -3,13 +3,15 @@ import UserResponseDto from "@/api/user/dto/userResponse.dto";
 export class CategoryResponseDTO {
   categoryId: string;
   title: string;
-  admin: UserResponseDto;
-  subAdmin: UserResponseDto[];
+  admin?: UserResponseDto;
+  subAdmin?: UserResponseDto[];
 
   constructor(params: ICategory) {
     this.categoryId = params.id;
     this.title = params.title;
-    this.admin = new UserResponseDto(params.admin);
-    this.subAdmin = params.subAdmin.map((user) => new UserResponseDto(user));
+    this.admin = params.admin ? new UserResponseDto(params.admin) : undefined;
+    this.subAdmin = params.subAdmin
+      ? params.subAdmin.map((user) => new UserResponseDto(user))
+      : undefined;
   }
 }
