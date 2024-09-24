5a9a2a99246dd399a3dd12fdf7f7c716f8b6dec5
cherry2407 <codus7958@gmail.com>
2024-09-24 14:55:11 +0900

    [김채연] 어드민유저 뷰라우터,뷰컨트롤러 작업

--- src/api/user/router/adminUser.router.ts
+++ src/api/user/router/adminUser.router.ts
@@ -13,56 +13,60 @@ import {
   adminUpdateUserValidator,
 } from "../dto/validation/adminUser.validation";
 import MongooseUserRepository from "../repository/mongooseUser.repository";
+import { MongooseProfileRepository } from "@/api/profile/repository/mongooseProfile.repository";
 
 const adminUserRouter = express.Router();
 
-/** 운영자 API 객체 */
-const USER_ROUTES = {
-  /** [운영자] 유저 전체 리스트 확인 */
+/** 관리자 유저 관련 API 경로 객체 */
+const ADMIN_USER_ROUTES = {
+  /** [관리자] 유저 전체 리스트 조회 */
   GET_USERS: `/admin-api/users`,
-  /** [운영자] 유저 상세 정보 확인 */
+  /** [관리자] 유저 상세 정보 조회 */
   GET_USER_DETAIL: `/admin-api/users/:userId`,
-  /** [운영자] 회원추가 */
+  /** [관리자] 유저 추가 */
   CREATE_USER: `/admin-api/users`,
-  /** [운영자] 유저 정보 수정 */
+  /** [관리자] 유저 정보 수정 */
   UPDATE_USER: `/admin-api/users/:userId`,
-  /** [운영자] 유저 삭제 */
+  /** [관리자] 유저 삭제 */
   DELETE_USER: `/admin-api/users/:userId`,
 } as const;
 
 const adminUserController = new AdminUserController(
-  new UserServiceImpl(new MongooseUserRepository())
+  new UserServiceImpl(
+    new MongooseUserRepository()
+    // new MongooseProfileRepository()
+  )
 );
 
 adminUserRouter.get(
-  extractPath(USER_ROUTES.GET_USERS, ROUTES_INDEX.ADMIN_USER_API),
+  extractPath(ADMIN_USER_ROUTES.GET_USERS, ROUTES_INDEX.ADMIN_USER_API),
   authRoleMiddleware(["admin"]),
   adminUserController.getAdminUsers
 );
 
 adminUserRouter.get(
-  extractPath(USER_ROUTES.GET_USER_DETAIL, ROUTES_INDEX.ADMIN_USER_API),
+  extractPath(ADMIN_USER_ROUTES.GET_USER_DETAIL, ROUTES_INDEX.ADMIN_USER_API),
   authRoleMiddleware(["admin"]),
   validate(adminGetUserDetailValidator),
   adminUserController.getAdminUserDetail
 );
 
 adminUserRouter.post(
-  extractPath(USER_ROUTES.CREATE_USER, ROUTES_INDEX.ADMIN_USER_API),
+  extractPath(ADMIN_USER_ROUTES.CREATE_USER, ROUTES_INDEX.ADMIN_USER_API),
   authRoleMiddleware(["admin"]),
   validate(adminCreateUserValidator),
   adminUserController.createAdminUser
 );
 
 adminUserRouter.put(
-  extractPath(USER_ROUTES.UPDATE_USER, ROUTES_INDEX.ADMIN_USER_API),
+  extractPath(ADMIN_USER_ROUTES.UPDATE_USER, ROUTES_INDEX.ADMIN_USER_API),
   authRoleMiddleware(["admin"]),
   validate(adminUpdateUserValidator),
   adminUserController.updateAdminUser
 );
 
 adminUserRouter.delete(
-  extractPath(USER_ROUTES.DELETE_USER, ROUTES_INDEX.ADMIN_USER_API),
+  extractPath(ADMIN_USER_ROUTES.DELETE_USER, ROUTES_INDEX.ADMIN_USER_API),
   authRoleMiddleware(["admin"]),
   validate(adminDeleteUserValidator),
   adminUserController.deleteAdminUser
