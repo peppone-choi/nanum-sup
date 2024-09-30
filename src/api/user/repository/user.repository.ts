b5e40cc76bd3a406826628f9e3ce920853ec97df
peppone-choi <peppone.choi@gmail.com>
2024-09-30 14:04:34 +0900

    [최병호] 중간 세이브.

--- src/api/user/repository/user.repository.ts
+++ src/api/user/repository/user.repository.ts
@@ -10,6 +10,10 @@ export interface UserRepository {
   /** DELETE USER */
   delete(id: string): Promise<void>;
   findByAccountId(accountId: string): Promise<IUser>;
+  getByNickname(nickname: string): Promise<IUser>;
+  existsByAccountId(accountId: string): Promise<boolean>;
+  existsByEmail(email: string): Promise<boolean>;
+  existsByNickname(nickname: string): Promise<boolean>;
 }
 
 // export default UserRepository;
